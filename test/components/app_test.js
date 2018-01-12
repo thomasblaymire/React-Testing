ed932645e406c044b1f4b376936d65901535dd21
Thomas Blaymire <thomas.blaymire@hermes-europe.co.uk>
2018-01-12 11:19:54 +0000

    Init base file with helpers

--- test/components/app_test.js
+++ test/components/app_test.js
@@ -3,13 +3,18 @@ import App from '../../src/components/app';
 
 // Use 'describe' to group together simular tests
 describe('App', () => {
+    let component;
 
-    // Use 'it' to test a single attribute of a target
-    it('shows the correct text', () => {
+    beforeEach(() => {
+        component = renderComponent(App);
+    });
 
-        // Use 'expect' to make an 'assertion' about a target
-        expect
+    it('shows a comment box', () =>{
+        expect(component.find('.comment-box')).to.exist;
+    });
 
+    it('shows a comment list', () =>{
+        expect(component.find('.comment-list')).to.exist;
     });
 
 });
---
+++ test/components/comment_box_test.js
@@ -0,0 +1,40 @@
+import { renderComponent, expect } from '../test_helper';
+import CommentBox from '../../src/components/comment_box';
+
+describe('CommentBox', () => {
+    //Aot so each function can reccieve it.
+    let component; // Undefined
+
+    // Before any of our if statements to inject CommentBox component into each. We re assign the compoennt.
+    beforeEach(() => {
+        component = renderComponent(CommentBox);
+    });
+
+    it('has the correct class', () => {
+        expect(component).to.have.class('comment-box');
+    })
+
+    it('has a text area', () => {
+        expect(component.find('textarea ')).to.exist;
+    });
+
+    it('has a button', () => {
+        expect(component.find('button')).to.exist;
+    });
+
+    describe('entering some text', () => {
+        // Will run before this specific describe block
+        beforeEach(() =>{
+            component.find('textarea').simulate('change', 'new comment');
+        });
+
+        it('shows that the text in the textarea', () => {
+            expect(component.find('textarea')).to.have.value('new comment');
+        });
+
+        it('when submitted, clears the input', () => {
+            component.simulate('submit');
+            expect(component.find('textarea')).to.have.value('');
+        });
+    });
+});
